cdc663d4378b6f5bece7c95115bcb16caaad0fd3
pojankolli <pojankolli@users.noreply.github.com>
2019-01-26 20:19:04 +0200

    Input versio
    Versio joka tunnistaa GET-pyynnön tietokoneen sisäisestä verkosta, muttei vielä toimi postmanin kanssa.

--- src/fleetmanagerMain/FleetManager.java
+++ src/fleetmanagerMain/FleetManager.java
@@ -1,34 +1,49 @@
 package fleetmanagerMain;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.StringTokenizer;
 
-public class FleetManager {
+public class FleetManager implements Runnable{
 	
 	private CarDatabaseHandler carHandler;
 	
-	public FleetManager() {
+	// server port
+	static final int PORT = 8090;
+	
+	
+	private ServerSocket server;
+	private Socket socket;
+	
+	public FleetManager(Socket socket, ServerSocket serverSocket) {
 		carHandler = new CarDatabaseHandler();
-		
-		//Following lines are for debug purpouses
-		//
-		Car testcar = new Car("Ford","Fiesta","LBD-013",2001,"23-01-2009",6,100);
-		
-		addCarToDatabase(testcar);
-		
-		displayCarListInDatabase(2000,2012,"Ford","");
-		listAllCars();
-		
-		removeCarFromDatabase(testcar);
-		//
-		//
-		
-		
-		
-		carHandler.closeConnection();
+		this.socket=socket;
+		this.server=serverSocket;
 	}
 
 	public static void main(String[] args) {
-		new FleetManager();
+		try {
+			ServerSocket server = new ServerSocket(PORT);
+			
+			FleetManager manager = new FleetManager(server.accept(),server);
+				
+			Thread thread = new Thread(manager);
+			thread.start();
+			
+			System.out.println("Server online");
+			
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	//adds car to database
@@ -76,5 +91,67 @@ public class FleetManager {
 			System.out.println(car.toString());
 		}
 	}
+	
+	@Override
+	public void run() {
+		BufferedReader in = null;
+		PrintWriter out = null;
+		BufferedOutputStream dataOut = null;
+		
+		try {
+			
+			// input stream
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			//output stream for headers
+			out = new PrintWriter(socket.getOutputStream());
+			
+			// output stream for requested data
+			dataOut = new BufferedOutputStream(socket.getOutputStream());
+			
+			//get first line of the request from the client (HTTP method)
+			String input = in.readLine();
+			System.out.println(input);
+				
+			//request parsing
+			StringTokenizer parse = new StringTokenizer(input);
+				
+			//HTTP method of the client
+			String method = parse.nextToken().toUpperCase(); 
+				
+				
+			// GET method
+			if (method.equals("GET")) {
+				
+				
+					System.out.println("GET method successful");
+						
+				}
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				//close all connections of FleetManager
+				in.close();
+				out.close();
+				dataOut.close();
+				socket.close();
+				
+				//TODO: these handled outside run-method
+				server.close();
+				carHandler.closeConnection();
+				
+			} catch (Exception e) {
+				System.err.println("Error closing stream : " + e.getMessage());
+			} 
+			System.out.println("Server offline");
+		}
+	}
+	
+	
+		
+	
 
 }
