2771cefda7cb277c69ce63dc330a83377e10b54c
pojankolli <pojankolli@users.noreply.github.com>
2019-01-26 11:27:45 +0200

    Projektin aloitus
    "Hello World" -testi ohjelman toiminnasta.

---
+++ src/fleetmanagerMain/FleetManager.java
@@ -0,0 +1,14 @@
+package fleetmanagerMain;
+
+public class FleetManager {
+
+	public FleetManager() {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		new FleetManager();
+	}
+
+}
