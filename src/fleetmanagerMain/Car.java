20186cf6c332f1e22f8cc0c39b56f6de1898fdba
pojankolli <pojankolli@users.noreply.github.com>
2019-01-26 12:28:17 +0200

    Framework
    aikainen framework

---
+++ src/fleetmanagerMain/Car.java
@@ -0,0 +1,18 @@
+package fleetmanagerMain;
+
+import java.util.Date;
+
+public class Car {
+	private String brand;
+	private String model;
+	private String licencePlate;
+	private int yearModel;
+	private Date inspectionDate;
+	private double engineSize;
+	private double enginePower;
+
+	public Car() {
+		
+	}
+
+}
--- src/fleetmanagerMain/FleetManager.java
+++ src/fleetmanagerMain/FleetManager.java
@@ -1,5 +1,7 @@
 package fleetmanagerMain;
 
+import java.util.ArrayList;
+
 public class FleetManager {
 
 	public FleetManager() {
@@ -7,8 +9,32 @@ public class FleetManager {
 	}
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
 		new FleetManager();
+	}
+	
+	//adds car to database
+	private void addCarToDatabase(Car car) {
+		
+	}
+	
+	//edits values of a car in database
+	private void editCarInDatabase(Car car, String[] args) {
+		
+	}
+	
+	//removes a car from database
+	private void removeCarFromDatabase(Car car) {
+	
+	}
+	
+	//displays the information of a particular car
+	private void displayCarInDatabase(Car car) {
+		
+	}
+	
+	//displays the information of a particular set of cars
+	private void displayCarListInDatabase(ArrayList<Car> cars) {
+		
 	}
 
 }
